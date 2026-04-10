67bd2b2dd91549f9ed737ab6430cd18dc1301a38
NAITIK RAJPUT <naitikrajput.26.04@gmail.com>
2026-04-10 14:32:20 +0530

    ALL

---
+++ arro.js
@@ -0,0 +1,7 @@
+//arrow fuction is a anonmyus function,fuction in a variable
+//let add =(x,y)=>{return x+y;} 
+//function(x,y){return x+y;}
+let add =(x,y)=>{console.log(x+y) } ;
+add(4,5)
+//object is represented by the help of curly brackets
+
